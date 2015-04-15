bccd113c95d252f6cc0a4ac69c3020c27e1d0643
Donovan Bray <donovan@homerun.com>
2015-04-15 11:18:51 -0700

    add methods to support rake and setting env

--- lib/cacert.rb
+++ lib/cacert.rb
@@ -1,8 +1,18 @@
 require "cacert/version"
 
 module Cacert
+
   # Returns the path of the embedded cacert.pem SSL certificates file.
   def self.pem
     File.join(File.dirname(__FILE__), '..', 'share', 'cacert.pem')
   end
+
+  def self.set_in_env
+    ENV['SSL_CERT_FILE'] = self.pem.to_s
+  end
+
+  def self.source
+  	"http://curl.haxx.se/ca/cacert.pem"
+  end
+
 end
--- test/cacert_test.rb
+++ test/cacert_test.rb
@@ -6,4 +6,11 @@ require 'cacert'
 describe Cacert do
   it('returns a filename') {Cacert.pem.must_match /cacert.pem/}
   it('points to a file') {File.exist?(Cacert.pem).must_equal true}
+  it('sets SSL_CERT_FILE') {
+  	Cacert.set_in_env
+  	ENV['SSL_CERT_FILE'].must_equal Cacert.pem
+  }
+  it('returns a url to cacert.pem') {
+  	Cacert.source.must_match /http.*\/cacert\.pem/
+  }
 end
