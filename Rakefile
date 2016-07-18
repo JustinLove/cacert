b42aca5d6c0357da1f77fb8a53a2d4b339108f0c
Ellen Marie Dash <me@duckie.co>
2016-07-18 05:44:42 -0400

    Use SSL to download file; request manual download otherwise.

--- Rakefile
+++ Rakefile
@@ -4,6 +4,14 @@ require "cacert"
 
 task :update do
   require "httparty"
+
+  if File.file?(Cacert.pem)
+    Cacert.set_in_env
+  else
+    warn "No cacert.pem found."
+    warn "Please manually download #{Cacert.source} and place it in #{Cacert.share_dir}."
+  end
+
   File.open(Cacert.pem, "wb") do |f|
     f.write HTTParty.get(Cacert.source).parsed_response
   end
--- lib/cacert.rb
+++ lib/cacert.rb
@@ -1,18 +1,24 @@
 require "cacert/version"
 
 module Cacert
+  # Returns the directory containing the cacert.pem file.
+  def self.share_dir
+    File.join(File.dirname(__FILE__), '..')
+  end
 
   # Returns the path of the embedded cacert.pem SSL certificates file.
   def self.pem
-    File.join(File.dirname(__FILE__), '..', 'share', 'cacert.pem')
+    File.join(share_dir, 'cacert.pem')
   end
 
+  # Sets the SSL_CERT_FILE environment variable to the location of the cacert.pem file.
   def self.set_in_env
     ENV['SSL_CERT_FILE'] = self.pem.to_s
   end
 
+  # The link which cacert.pem can be downloaded from.
   def self.source
-  	"http://curl.haxx.se/ca/cacert.pem"
+    "https://curl.haxx.se/ca/cacert.pem"
   end
 
 end
